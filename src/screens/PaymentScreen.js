b55240a1fe69069d984460e05025aba675e02a30
Florentpop <Florentpop@gmail.com>
2021-04-27 05:18:56 +0000

    Payment Screen

--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -6,7 +6,10 @@ import {
   TouchableOpacity,
   Alert,
   StyleSheet,
+  Image,
+  ImageBackground,
 } from "react-native";
+import * as Animatable from "react-native-animatable";
 import { ScrollView } from "react-native-gesture-handler";
 
 import { PayWithFlutterwave } from "flutterwave-react-native";
@@ -22,51 +25,62 @@ function PaymentScreen({ navigation, route }) {
 
   return (
     <View style={styles.container}>
-      <KeyboardAwareScrollView style={styles.container}>
-        <View style={styles.contentContainer}>
-          <Text style={styles.amountText}>
-            GH{"\u20B5"}
-            {route.params.data}
-          </Text>
-        </View>
+      <ImageBackground
+        source={require("../../assets/Safepay.png")}
+        style={styles.header}
+      >
+        <Text style={styles.text_header}>Bringing the Gap of Trust</Text>
+      </ImageBackground>
+      <Animatable.View animation="fadeInLeft" style={styles.footer}>
+        <KeyboardAwareScrollView style={styles.ontainer}>
+          <ImageBackground
+            source={require("../../assets/Safepay.png")}
+            style={styles.contentContainer}
+          >
+            <Text style={styles.amountText}>
+              GH{"\u20B5"}
+              {route.params.data}
+            </Text>
+          </ImageBackground>
 
-        <PayWithFlutterwave
-          onRedirect={(res) => {
-            const status = res.status;
-            if (status === "successful") {
-              Alert.alert(
-                `Your Transaction with ID ${res.transaction_id} was Successful`
-              );
-            } else {
-              return Alert.alert("Something Went Wrong");
-            }
-          }}
-          options={{
-            tx_ref: transactionID,
-            authorization: "FLWPUBK_TEST-a232cbe6c1595c2d05c81e28624a905d-X",
-            customer: {
-              email: "customer-email@example.com",
-            },
-            amount: payAmount,
-            currency: "GHS",
-            payment_options: "card",
-          }}
-          customButton={(props) => (
-            <TouchableOpacity
-              onPress={props.onPress}
-              isBusy={props.isInitializing}
-              disabled={false}
-            >
-              <View style={styles.confirmOpacity}>
-                <Text style={styles.confirmText}>
-                  {/*Top Up {route.params.data}*/}
-                  Make Payment
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        />
-      </KeyboardAwareScrollView>
+          <PayWithFlutterwave
+            onRedirect={(res) => {
+              const status = res.status;
+              if (status === "successful") {
+                Alert.alert(
+                  `Your Transaction with ID ${res.transaction_id} was Successful`
+                );
+              } else {
+                return Alert.alert("Something Went Wrong");
+              }
+            }}
+            options={{
+              tx_ref: transactionID,
+              authorization: "FLWPUBK_TEST-a232cbe6c1595c2d05c81e28624a905d-X",
+              customer: {
+                email: "customer-email@example.com",
+              },
+              amount: payAmount,
+              currency: "GHS",
+              payment_options: "card",
+            }}
+            customButton={(props) => (
+              <TouchableOpacity
+                onPress={props.onPress}
+                isBusy={props.isInitializing}
+                disabled={false}
+              >
+                <View style={styles.confirmOpacity}>
+                  <Text style={styles.confirmText}>
+                    {/*Top Up {route.params.data}*/}
+                    Make Payment
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </KeyboardAwareScrollView>
+      </Animatable.View>
     </View>
   );
 }
@@ -74,21 +88,32 @@ function PaymentScreen({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
+    //paddingTop: 40,
     //justifyContent: "space-evenly",
+    backgroundColor: "#06C8F4",
   },
 
   contentContainer: {
     alignSelf: "center",
     height: 150,
     width: 300,
-    backgroundColor: "#00C598",
-    borderRadius: 10,
-    elevation: 45,
+    //backgroundColor: "#00C598",
+    borderRadius: 20,
+    //elevation: 45,
     //shadowColor: "black",
     shadowColor: "grey",
     shadowOpacity: 0.27,
     shadowRadius: 10.65,
+    //paddingTop: -80,
+  },
+
+  image: {
+    alignSelf: "center",
+    height: 150,
+    width: 300,
+    //backgroundColor: "#00C598",
+    borderRadius: 10,
+    elevation: 45,
   },
 
   amountText: {
@@ -96,17 +121,17 @@ const styles = StyleSheet.create({
     textAlign: "center",
     paddingTop: 55,
     fontWeight: "bold",
-    color: "#0f184b",
+    color: "#fff",
   },
 
   confirmOpacity: {
-    backgroundColor: "#5CBE7D",
+    backgroundColor: "#06C8F4",
     color: "#fff",
     width: 170,
     height: 50,
     borderRadius: 25,
     alignSelf: "center",
-    marginTop: 80,
+    marginTop: 105,
   },
 
   confirmText: {
@@ -115,6 +140,29 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     fontWeight: "bold",
     fontSize: 18,
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 25,
+    paddingTop: 25,
+  },
+
+  text_header: {
+    color: "#fff",
+    //fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+
+  footer: {
+    flex: 15,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 80,
   },
 });
 
