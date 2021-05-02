5ad96725a20117d179d6159a51c26a0df6d3a10e
Florentpop <Florentpop@gmail.com>
2021-05-02 17:19:02 +0000

    work on History Card

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import moment from "moment";
-import { View, Text, ScrollView, StyleSheet, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
 class History extends Component {
   constructor(props) {
@@ -13,19 +20,25 @@ class History extends Component {
     return (
       <View style={styles.mainContainer}>
         <StatusBar barStyle="dark-content" backgroundColor="#06C8F4" />
-        <View style={styles.contentContainer}>
-          <Text style={styles.amountText}>{this.props.item.itemName}</Text>
-          <Text style={styles.dateTime}>
-            GH{"\u20B5"}
-            {this.props.item.overAllPayment}
-          </Text>
-          <Text style={styles.totalText}>{this.props.item.companyName}</Text>
-          <Text style={styles.dateTime}>{this.props.item.sellerNumber}</Text>
-          <Text style={styles.dateTime}>{this.props.item.itemDescription}</Text>
-          <Text style={styles.dateTime}>
-            {moment(this.props.item.createdAt.toDate()).calendar()}
-          </Text>
-        </View>
+        <TouchableOpacity>
+          <View style={styles.contentContainer}>
+            <Text style={styles.ItemName}>{this.props.item.itemName}</Text>
+
+            <Text style={styles.Amount}>
+              GH{"\u20B5"}
+              {this.props.item.overAllPayment}
+            </Text>
+
+            <Text style={styles.totalText}>{this.props.item.companyName}</Text>
+            <Text style={styles.number}>{this.props.item.sellerNumber}</Text>
+            <Text style={styles.Descrip}>
+              {this.props.item.itemDescription}
+            </Text>
+            <Text style={styles.dateTime}>
+              {moment(this.props.item.createdAt.toDate()).calendar()}
+            </Text>
+          </View>
+        </TouchableOpacity>
 
         <View style={styles.horizontalLine} />
       </View>
@@ -47,11 +60,13 @@ const styles = StyleSheet.create({
     width: 330,
     backgroundColor: "#fff",
     borderRadius: 10,
-    elevation: 25,
+    elevation: 10,
     //shadowColor: "black",
     shadowColor: "blue",
     shadowOpacity: 5.27,
     shadowRadius: 4.65,
+    paddingLeft: 15,
+    paddingTop: 10,
   },
 
   amountText: {
@@ -60,7 +75,7 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     paddingTop: 5,
     textAlign: "left",
-    color: "#436768",
+    //color: "#436768",
   },
 
   totalText: {
@@ -68,19 +83,67 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingBottom: 5,
     textAlign: "left",
-    color: "#436768",
+    //color: "#436768",
   },
 
   dateTime: {
+    fontSize: 14,
+    paddingRight: 10,
+    color: "#436768",
+    //textAlign: "left",
+    //paddingBottom: 5,
+    alignSelf: "flex-end",
+  },
+
+  Descrip: {
+    fontSize: 16,
+    paddingRight: 15,
+  },
+
+  title: {
+    fontSize: 18,
+    paddingRight: 15,
+    // color: "#436768",
+    //textAlign: "left",
+    //paddingBottom: 5,
+    // alignSelf: "flex-start",
+
+    //marginBottom: 10,
+  },
+
+  Amount: {
+    fontSize: 18,
+    paddingRight: 35,
+    //color: "#436768",
+    //textAlign: "left",
+    //paddingBottom: 5,
+    // alignContent: "flex-end",
+    alignSelf: "flex-end",
+    //textAlign: "center",
+    //marginBottom: 10,
+  },
+
+  number: {
+    fontSize: 18,
+    paddingRight: 35,
+    //color: "#436768",
+    //paddingBottom: 5,
+    // alignContent: "flex-end",
+    alignSelf: "flex-end",
+    //textAlign: "center",
+    //marginBottom: 10,
+  },
+
+  ItemName: {
     fontSize: 18,
     paddingLeft: 10,
-    color: "#436768",
+    //color: "#436768",
     textAlign: "left",
     paddingBottom: 5,
   },
 
   horizontalLine: {
-    borderBottomColor: "#ff70d9",
+    borderBottomColor: "#fff", //"#ff70d9",
     borderBottomWidth: 1,
     width: 330,
     alignSelf: "center",
